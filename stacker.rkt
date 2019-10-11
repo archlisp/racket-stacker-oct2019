d74ba6f18763d0cc2a9a51bd5ac7e38d6d42ffb4
Christopher Mark Gore <chris.gore@rate.com>
2019-10-10 20:24:54 -0500

    stack stuff

--- stacker.rkt
+++ stacker.rkt
@@ -26,5 +26,26 @@
 
 (define-macro (stacker-module-begin HANDLE-EXPR ...)
   #'(#%module-begin
-     'HANDLE-EXPR ...))
+     HANDLE-EXPR ...
+     (display (first stack))))
 (provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? * arg) (equal? + arg))
+     (define op-result (arg (pop-stack!) (pop-stack!))) 
+     (push-stack! op-result)]))
+(provide handle)
+
+(provide + *)
