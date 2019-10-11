cf8e87c3a214870938b36a7e6c6cfc29658e5c08
Christopher Mark Gore <chris.gore@rate.com>
2019-10-10 19:31:21 -0500

    the answer

---
+++ stacker.rkt
@@ -0,0 +1,11 @@
+#lang br/quicklang
+
+;;;; `C-c C-z` to launch your REPL from one of the rkt files.
+;;;; `C-c C-k` to compile and load your file in the repl.
+;;;; `C-x C-e` will evaluate the form and show it in the debug buffer.
+;;;; `C-c C-c` will evaluate the form and show in the Emacs gutter.
+;;;; Geiser Cheat Sheet: http://www.nongnu.org/geiser/geiser_5.html
+
+
+42
+
